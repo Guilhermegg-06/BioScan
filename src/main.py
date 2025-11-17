9890f0de782689de9bff93ab62337751235bd56d
Guilherme gg <ggdev0001@gmail.com>
2025-11-17 20:35:20 -0300

    iniciando projeto BioScan

---
+++ src/main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from models.amostra import Amostra
+from models.analise import Analise
+from models.interpretador import Interpretador
+from models.visualizador import Visualizador
+from utils.exceptions import ExcecaoAmostraInvalida
+
+def main():
+    print("=== BioScan – Sistema de Análise Científica ===")
+
+    try:
+        df = pd.read_csv("data/amostras.csv")
+
+        # Criação das amostras
+        amostras = []
+        for _, row in df.iterrows():
+            amostras.append(
+                Amostra(
+                    id=row["ID"],
+                    glicose=row["Glicose"],
+                    proteina=row["Proteina"],
+                    colesterol=row["Colesterol"]
+                )
+            )
+
+        print(f"[OK] {len(amostras)} amostras carregadas.")
+
+        # Análise
+        analise = Analise(amostras)
+        medias = analise.medias()
+        desvios = analise.desvios()
+
+        print("\n=== Estatísticas Gerais ===")
+        print("Médias:", medias)
+        print("Desvios:", desvios)
+
+        # Interpretação
+        interpretador = Interpretador(amostras)
+        resultados = interpretador.interpretar()
+
+        print("\n=== Classificação das Amostras ===")
+        for res in resultados:
+            print(f"Amostra {res[0]} → {res[1]}")
+
+        # Visualização
+        visualizador = Visualizador(df)
+        print("\nGerando gráficos...")
+        visualizador.exibir()
+
+    except ExcecaoAmostraInvalida as e:
+        print(e)
+    except FileNotFoundError:
+        print("[ERRO] Arquivo amostras.csv não encontrado em /data.")
+    except Exception as e:
+        print("[ERRO GERAL]", e)
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/interfaces.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class Exibivel(ABC):
+    @abstractmethod
+    def exibir(self):
+        pass
